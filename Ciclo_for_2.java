d5646032bfd0ef751379d5a5597ce4d7e4199672
SirArthuro <142696770+SirArthuro@users.noreply.github.com>
2023-08-20 22:41:31 -0500

    Add files via upload

---
+++ Ciclo_for_2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author SirArthur
+ * el siguiente ejemplo es una variable del ciclo while, pero utilizando el ciclo for
+ */
+public class Ciclo_for_2 {
+    public static void main(String args[])
+    {
+        for(int i=1;i<=50;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
+
