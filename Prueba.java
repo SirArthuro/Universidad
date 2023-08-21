ad7d24fca928022729b2ea103885f2c81b575d36
SirArthuro <142696770+SirArthuro@users.noreply.github.com>
2023-08-20 20:27:23 -0500

    Add files via upload

---
+++ Prueba.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba;
+
+import static java.time.Clock.system;
+import static java.time.InstantSource.system;
+
+/**
+ *
+ * @author SirArthur
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        
+        int i = 0;
+        while (i < 10) {
+            System.out.println("i: " + i);
+            i++;}
+        
+        
+    }
+}
