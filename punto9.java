5631d3fa880040c1ce66880fc9534c38790c3011
SirArthuro <142696770+SirArthuro@users.noreply.github.com>
2023-09-03 13:16:15 -0500

    punto9.java

--- punto9.java
+++ punto9.java
@@ -15,7 +15,9 @@ public class punto9 {
      */
     public static void main(String[] args) {
         //Decalaracion de variables
-       int ne=5,i=0,vta=0,vtb=0,vtc=0,vtd=0,vtm=0;
+       int ne=5,i=0,vta=0,vtb=0,vtc=0,vtd=0,vtca=0,vtcb=0,vtcc=0,vtcd=0,vtce=0;
+       double vtm=0;
+       
         int [] c = new int [ne];
         int [] ca = new int [ne];
         int [] cb = new int [ne];
@@ -34,36 +36,72 @@ public class punto9 {
         }
         //calculo para saber el numero de votos por candidato
         for(i=0;i<ne;i++){
-            vta=ca[i];
-            vtb=cb[i];
-            vtc=cc[i];
-            vtd=cd[i];}
-                //Calculo de mayor votacion por comuna
+            vta=vta+ca[i];
+            vtb=vtb+cb[i];
+            vtc=vtc+cc[i];
+            vtd=vtd+cd[i];}
+        
+        
+        //calculo para saber el numero de votos por columna
+         for(i=0;i<ne;i++){
+          vtca=(ca[i]+cb[i]+cc[i]+cd[i]);
+            for(i=1;i<ne;i++){
+              vtcb=(ca[i]+cb[i]+cc[i]+cd[i]);
+               for(i=2;i<ne;i++){
+                vtcc=(ca[i]+cb[i]+cc[i]+cd[i]);
+                    for(i=3;i<ne;i++){
+                     vtcd=(ca[i]+cb[i]+cc[i]+cd[i]);
+                        for(i=3;i<ne;i++){
+                         vtce=(ca[i]+cb[i]+cc[i]+cd[i]);
+                        }
+                    }
+                  }
+               }
+            }
+        
+         
+         
+        
+          
+        
+        
+        //calculo de mayo numero de votaciones por columna
+        if((vtca>vtcb)&&(vtca>vtcc)&&(vtca>vtcd)&&(vtca>vtce)){
+            System.out.println("La mayor cantidad de votos fue en la comuna 1 con: "+vtca);
+        }else if((vtcb>vtca)&&(vtcb>vtcc)&&(vtcb>vtcd)&&(vtcb>vtce)){
+            System.out.println("La mayor cantidad de votos fue en la comuna 2 con: "+vtcb);
+        }else if((vtcc>vtcb)&&(vtcc>vtca)&&(vtcc>vtcd)&&(vtcc>vtce)){
+             System.out.println("La mayor cantidad de votos fue en la comuna 3 con: "+vtcc);
+        }else if((vtcd>vtcc)&&(vtcd>vtcb)&&(vtcd>vtca)&&(vtcd>vtce)){
+             System.out.println("La mayor cantidad de votos fue en la comuna 4 con: "+vtcd);
+        }else if((vtce>vtcd)&&(vtce>vtcc)&&(vtce>vtcb)&&(vtce>vtca)){
+             System.out.println("La mayor cantidad de votos fue en la comuna 5 con: "+vtce);
+        }
+        
+                //Calculo de mayor votacion por candidato
                 if((vta>vtb)&&(vta>vtc)&&(vta>vtd)){
-                    System.out.println("La mayor cantidad de votos fue en la comuna 1 con: "+vta);
-                }else if((vtb>vta)&&(vtb>vtc)&&(vtb>vtd)){
-                    System.out.println("La mayor cantidad de votos fue en la comuna 2 con: "+vtb);
-                }else if((vtc>vtb)&&(vtc>vta)&&(vtc>vtd)){
-                    System.out.println("La mayor cantidad de votos fue en la comuna 3 con: "+vtc);
+                }else if((vtb>vta)&&(vtb>vtc)&&(vtb>vtd)){                   
+                }else if((vtc>vtb)&&(vtc>vta)&&(vtc>vtd)){                    
                 }else if((vtd>vtc)&&(vtd>vtb)&&(vtd>vta)){
-                    System.out.println("La mayor cantidad de votos fue en la comuna 4 con: "+vtd);
                 }
             //calculo para saber el 50% del total de votaciones
-            vtm=((vta+vtb+vtc+vtd)*50)/100;
+            vtm=(((vta+vtb+vtc+vtd)*50)/100);
            
             //calculo para saber quien gano
-            if(vta>vtm){
+            if((vta>vtb)&&(vta>vtc)&&(vta>vtd)){
                 System.out.println("El ganador fue el candidato a");
-            }else if(vtb>vtm){
+            }else if((vtb>vta)&&(vtb>vtc)&&(vtb>vtd)){
+                
                 System.out.println("El ganador fue el candidato b");
-            }else if(vtc>vtm){
+            }else if((vtc>vta)&&(vtc>vtb)&&(vtc>vtd)){
+                
                 System.out.println("El ganador fue el candidato c");
-            }else if(vtd>vtm){
-                 System.out.println("El ganador fue el candidato a");
+            }else if((vtd>vtc)&&(vtd>vtb)&&(vtd>vta)){
+                 
+                 System.out.println("El ganador fue el candidato d");
             }
         System.out.println("El total de votos fue de: "+(vta+vtb+vtc+vtd));
        
     } 
 }
     
-
